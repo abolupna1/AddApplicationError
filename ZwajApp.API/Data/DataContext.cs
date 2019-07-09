51eebb64aa996fe8b275a5d7272a46c86ad5a2fb
= <=>
2019-07-09 16:09:57 +0300

    addAblicationError

---
+++ ZwajApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Models;
+
+namespace ZwajApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+    }
+}
---
+++ ZwajApp.API/Models/User.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace ZwajApp.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] passwordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string  KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActivate { get; set; }
+        public string  Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+    }
+}
